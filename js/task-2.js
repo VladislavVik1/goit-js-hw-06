42d16743822adaacd0510256abd0a8fc980ddc9d
Vik-ing-ing <Dakraman1232@ukr.net>
2025-02-06 17:22:15 +0200

    1

--- js/task-2.js
+++ js/task-2.js
@@ -1,18 +1,19 @@
 class Storage {
+  #items;
   constructor(initialItems) {
-    this.items = initialItems;
+    this.#items = initialItems;
   }
 
   getItems() {
-    return this.items;
+    return this.#items;
   }
 
   addItem(newItem) {
-    this.items.push(newItem);
+    this.#items.push(newItem);
   }
 
   removeItem(itemToRemove) {
-    this.items = this.items.filter(item => item !== itemToRemove);
+    this.#items = this.#items.filter(item => item !== itemToRemove);
   }
 }
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
